314d5f7b9df54093fbb89e6d28a3938d871b9306
jaeyeon <lj_yeon_@naver.com>
2023-11-01 11:12:14 +0900

    RegionalList 추가

--- Front-end/this_is_osaka/src/pages/RegionalList.js
+++ Front-end/this_is_osaka/src/pages/RegionalList.js
@@ -2,7 +2,28 @@ import './css/RegionalList.css';
 // import { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
 
+
+const RegionalListItem = () => {
+    const regionalList = [ '오사카만 지역','도톤보리 & 신사이바시', '남바', '덴노지사원',
+                            '오사키시 북부', '오사카성과 그 주변', '사카이 & 기시와다', '이케다']
+    const reginoalListItem = regionalList.map((object)=>{
+        return(
+            <button>
+                <Link to="/a">{object}</Link>
+            </button>
+        )
+    })
+    return reginoalListItem;
+}
+
 const RegionalList = () => {
+    return(
+        <div className="RegionalList">
+            지역 리스트
+            <RegionalListItem/>
+        </div>
+    )  
+    /*
     return (
         <div className="RegionalList">
             지역 리스트
@@ -11,6 +32,7 @@ const RegionalList = () => {
             </button>
         </div>
     );
+    */
 };
 
 
