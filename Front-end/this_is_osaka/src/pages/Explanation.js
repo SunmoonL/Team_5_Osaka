a682cbdd3d02881c247ec93205958f5a24ed3376
jaeyeon <lj_yeon_@naver.com>
2023-11-06 11:32:34 +0900

    detail css, explan js

--- Front-end/this_is_osaka/src/pages/Explanation.js
+++ Front-end/this_is_osaka/src/pages/Explanation.js
@@ -1,14 +1,23 @@
 import './scss/Explanation.scss';
 import { useState, useEffect } from 'react';
 
-const Explanation = ({children, detailTitle, detailContent, imgFolder}) => {
+const GetDB = async()=>{
+    const url = `http://kkms4001.iptime.org:10093/question_list/`
+    const response = await fetch(url);
+    const data = await response.json();
+    let needData = await Object.values(data)[0]
+    return needData;
+};
 
+const Explanation = ({children, detailTitle, detailContent, imgFolder}) => {
+    
     const [changeCategory, setChangeCategory] = useState("")
     const questionImgSrc = `${process.env.PUBLIC_URL}/images/${imgFolder}/${changeCategory}/2.Takoyasu.jpg`
-
     const categoryList = [{food:'음식'}, {hotel:'호텔'}, {place:'관광지'}] 
     
     const mouseClickFunc= (key, i) =>{
+        console.log(questionImgSrc)
+        console.log(GetDB().first_link)
         setChangeCategory(key) 
         const questionList = document.getElementById("questionList").children;
         questionList[i].classList.add('colorChange')
@@ -17,7 +26,7 @@ const Explanation = ({children, detailTitle, detailContent, imgFolder}) => {
         const saveList = [];
         categoryList.forEach((value, i)=>{
             for(let key in value){
-                saveList.push(<li onClick={ () =>  mouseClickFunc(key, i) }>{value[key]}</li>)
+                saveList.push(<li key={i} onClick={ () =>  mouseClickFunc(key, i) }>{value[key]}</li>)
             }
         })
         return saveList;
@@ -44,16 +53,17 @@ const Explanation = ({children, detailTitle, detailContent, imgFolder}) => {
         };
         return listItem;
     }
-
+    /*
     if (detailContent !== "" && detailTitle !== "") {
         console.log(detailContent);
     }
-
+    */
     return (
         <div className="Explanation">
             <h3>{detailTitle}</h3>
             <p>{detailContent}</p>
             <ul id="questionList">
+                
                 {QuestionList()}
             </ul>
             <div className="questionImg">
--- Front-end/this_is_osaka/src/pages/RegionalList.js
+++ Front-end/this_is_osaka/src/pages/RegionalList.js
@@ -6,21 +6,21 @@ import Explanation from './Explanation';
 
 const RegionalList = ( {changeBackground, changePrevBackground, setDetailTitle, setDetailContent, setImgFolder} ) => {
    const regionaArray = [ { title:"오사카만", content:"수족관, 세계 수준의 놀이공원, 유니버설 스튜디오 재팬 등이 있는 최고의 관광지",
-                            imgFolder:"osaka_man", backgroundSrc:"osaka_port.jpg"},
+                            imgFolder:"osaka_port", backgroundSrc:"osaka_port.jpg"},
                             { title:"도톤보리", content: "휘황찬란한 빛과 생동감, 대도시 오사카의 풍류",
-                            imgFolder:"osaka_man", backgroundSrc:"dotonbori.jpg"},
+                            imgFolder:"dotonbori", backgroundSrc:"dotonbori.jpg"},
                             { title:"난바", content:"미식가, 애니메이션 매니아, 예술가들을 위한 장소",
-                            imgFolder:"osaka_man", backgroundSrc:"nanba.jpg"},
+                            imgFolder:"nanba", backgroundSrc:"nanba.jpg"},
                             { title:"시텐노지사원", content:"오사카의 옛 도심의 부활",
-                            imgFolder:"osaka_man", backgroundSrc:"Tennoji_temple.jpg"},
+                            imgFolder:"shitennogi", backgroundSrc:"Tennoji_temple.jpg"},
                             { title:"오사카시북부", content:"신오사카와 오사카/우메다역 인근의 식당과 쇼핑 공간",
-                            imgFolder:"osaka_man", backgroundSrc:"osaka_north.jpg"},
+                            imgFolder:"osaka_north", backgroundSrc:"osaka_north.jpg"},
                             { title:"오사카성", content:"도시를 떠나 휴식하기 좋은 울창한 숲이 있는 공원 ",
-                            imgFolder:"osaka_man", backgroundSrc:"osaka_castle_2.jpg"},
+                            imgFolder:"osaka_castle", backgroundSrc:"osaka_castle_2.jpg"},
                             { title:"사카이 & 기시와다", content:"오사카에 인접한 5세기 거대고분",
-                            imgFolder:"osaka_man", backgroundSrc:"sakai.jpg"},
+                            imgFolder:"sakai&kisiwada", backgroundSrc:"sakai.jpg"},
                             { title:"이케다", content:"오사카 북부의 자연 트레킹 및 브로드웨이식 공연 감상",
-                            imgFolder:"osaka_man", backgroundSrc:"ikeda.jpg"},
+                            imgFolder:"ikeda", backgroundSrc:"ikeda.jpg"},
                         ];
     const saveList = [];
     const [ BackgroundSrc, setBackground ] = changeBackground;
