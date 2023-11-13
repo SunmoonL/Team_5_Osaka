097d021f1e0fd0f52e2491abae6aafc643b17dfa
ehfehfdl0927 <kimjoohe10@naver.com>
2023-11-13 15:36:00 +0900

    d

--- Back-end/osaka_app/filter_keyword.py
+++ Back-end/osaka_app/filter_keyword.py
@@ -1,5 +1,5 @@
 filter_keyword_list = [
-    {"filtered_str" : "라면", "correct_str": "라멘"},
+    {"filtered_str" : "라면 라맨", "correct_str": "라멘"},
     {"filtered_str" : "타코야끼 타코야키 타꼬야끼 타고야키 타꼬야키 다코야키 다꼬야끼 다고야키 다꼬야키", "correct_str": "다코야끼"},
     {"filtered_str" : "횟 사시미", "correct_str": "회"},
     {"filtered_str" : "스시", "correct_str": "초밥"},
